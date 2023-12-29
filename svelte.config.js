f3f2f19b34a61e71e982353ba8306d30de9a99a5
Karl Sims <karlsims.uk@gmail.com>
2023-12-29 02:31:36 +0000

    update config

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-// import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
