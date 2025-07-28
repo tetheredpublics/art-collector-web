96dec827f1f27f3cd8db75c9a3df1504310dfa33
Karl Sims <karlsims.uk@gmail.com>
2025-07-28 02:27:50 +0100

    fix portal

--- src/components/Popover.svelte
+++ src/components/Popover.svelte
@@ -1,6 +1,7 @@
 <!-- Popover.svelte -->
 <script>
     import { onDestroy } from 'svelte';
+    import {  portal} from "svelte-portal";
 
     export let visible = false;
 
@@ -30,7 +31,7 @@
 
 {#if visible}
 <!-- svelte-ignore a11y-no-static-element-interactions -->
-<div class="bg-black/50 fixed z-[2] flex justify-center items-center !mx-0 top-0 right-0 bottom-0 left-0">
+<div use:portal={"body"} class="bg-black/50 fixed z-[999] flex justify-center items-center !mx-0 !top-0 !right-0 !bottom-0 !left-0">
     <div class="popover border-2 border-slate-500 z-[5] bg-white p-2 relative container rounded-lg max-w-full md:max-w-sm">
         <div class="text-right">
             <button class="hover:cursor-pointer" on:click={closePopover}>
